c117017508dadeb8cb74c52dc1c81e435ba3eb9a
Naman Patel <namanpatel5@outlook.com>
2021-02-11 15:57:37 +0000

    commit everything

---
+++ public/js/clientSocket.js
@@ -0,0 +1,30 @@
+var connected = false;
+
+var socketUrl = "https://hyperlynk-beta.herokuapp.com"; // ADD YOUR HOSTED URL HERE
+     
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    // If in localhost, use this url
+    socketUrl = "https://hyperlynk-beta.herokuapp.com";
+}
+ 
+var socket = io(socketUrl);
+
+
+
+socket.emit("setup", userLoggedIn);
+
+socket.on("connected", () => connected = true);
+socket.on("message received", (newMessage) => messageReceived(newMessage));
+
+socket.on("notification received", () => {
+    $.get("/api/notifications/latest", (notificationData) => {
+        showNotificationPopup(notificationData)
+        refreshNotificationsBadge();
+    })
+})
+
+function emitNotification(userId) {
+    if(userId == userLoggedIn._id) return;
+
+    socket.emit("notification received", userId);
+}
